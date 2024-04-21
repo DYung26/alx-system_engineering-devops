325518407f589459ead732a1e9e282dd119be64d
DYung26 <theadstore1407@gmail.com>
2024-04-21 16:48:07 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,20 @@
+# ensure '/etc/ssh/ssh_config' exists
 file { '/etc/ssh/ssh_config':
   ensure => present,
 }
 
+# 'Enable Password Authentication'
 file_line { 'Enable Password Authentication':
-  ensure  => present,
-  path    => '/etc/ssh/sshd_config',
-  line    => '#  PasswordAuthentication no',
-  match   => '^#  PasswordAuthentication',
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line   => '#  PasswordAuthentication no',
+  match  => '^#  PasswordAuthentication',
 }
 
+# Declare identity file
 file_line { 'IdentityFile':
-  ensure  => present,
-  path    => '/etc/ssh/sshd_config',
-  line    => '#  IdentityFile ~/.ssh/school',
-  match   => '^#  IdentityFile ~/.ssh/school',
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line   => '#  IdentityFile ~/.ssh/school',
+  match  => '^#  IdentityFile ~/.ssh/school',
 }    
