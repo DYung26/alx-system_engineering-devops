052112bfdb8da5ee2c572a5666594cd996e284d0
DYung26 <theadstore1407@gmail.com>
2024-04-21 17:54:31 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,23 @@
+exec { 'install_stdlib_module':
+  command     => '/opt/puppetlabs/bin/puppet module install puppetlabs-stdlib',
+  path        => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+  logoutput   => true,
+  refreshonly => true,
+}
+
+include  stdlib
+
 # ensure '/etc/ssh/ssh_config' exists
 file { '/etc/ssh/ssh_config':
   ensure => present,
 }
 
-# 'Enable Password Authentication'
+# Enable Password Authentication
 file_line { 'Enable Password Authentication':
   ensure => present,
   path   => '/etc/ssh/sshd_config',
   line   => '#  PasswordAuthentication no',
-  match  => '^#  PasswordAuthentication',
+  match  => '#  PasswordAuthentication',
 }
 
 # Declare identity file
@@ -16,5 +25,5 @@ file_line { 'IdentityFile':
   ensure => present,
   path   => '/etc/ssh/sshd_config',
   line   => '#  IdentityFile ~/.ssh/school',
-  match  => '^#  IdentityFile ~/.ssh/school',
+  match  => '#  IdentityFile ~/.ssh/school',
 }    
