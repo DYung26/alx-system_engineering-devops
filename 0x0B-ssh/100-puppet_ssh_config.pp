011534565b762497a79c4d07a9fc10122c63ff69
DYung26 <theadstore1407@gmail.com>
2024-04-21 20:05:17 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,29 +1,13 @@
-exec { 'install_stdlib_module':
-  command     => '/opt/puppetlabs/bin/puppet module install puppetlabs-stdlib',
-  path        => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
-  logoutput   => true,
-  refreshonly => true,
+# Client configuration
+exec { 'Turn off passwd auth':
+  command => 'bash -c "echo PasswordAuthentication no >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
-
-include  stdlib
-
-# ensure '/etc/ssh/ssh_config' exists
-file { '/etc/ssh/ssh_config':
-  ensure => present,
+exec { 'Declare identity file':
+  command => 'bash -c "echo IdentityFile \'~/.ssh/school\' >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
-
-# Enable Password Authentication
-file_line { 'Enable Password Authentication':
-  ensure => present,
-  path   => '/etc/ssh/sshd_config',
-  line   => '#  PasswordAuthentication no',
-  match  => '#  PasswordAuthentication',
+exec { 'Turn on pubkey auth':
+  command => 'bash -c "echo PubkeyAuthentication yes >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
-
-# Declare identity file
-file_line { 'IdentityFile':
-  ensure => present,
-  path   => '/etc/ssh/sshd_config',
-  line   => '#  IdentityFile ~/.ssh/school',
-  match  => '#  IdentityFile ~/.ssh/school',
-}    
