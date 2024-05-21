494a06dcf6030bc0b3a60194e523cbfb2ea53cdc
DYuŋ <80885248+DYung26@users.noreply.github.com>
2024-05-21 05:59:10 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""
-A script to fetch user data and their to-do lists from the
-JSONPlaceholder API, and write the data to a JSON file.
-"""
+"""Exports to-do list information of all employees to JSON format."""
+import json
+import requests
 
 if __name__ == "__main__":
-    import json
-    import requests
-    import sys
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    r = requests.get(f'{base_url}/users/').json()
-    json_data = {}
-    output_file = "todo_all_employees.json"
-    task_list = []
-
-    for user in r:
-        user_id = user["id"]
-        username = user["username"]
-        r_todo = requests.get(f'{base_url}/users/{user_id}/todos').json()
-        task_dict = {}
-
-        for todo in r_todo:
-            task = todo["title"]
-            status = todo["completed"]
-
-            task_dict.update({"username": username,
-                              "task": task,
-                              "completed": status
-                              })
-        task_list.append(task_dict)
-        json_data.update({f"{user_id}": task_list})
-    with open(output_file, 'w', newline='') as jsonfile:
-        json.dump(json_data, jsonfile)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
