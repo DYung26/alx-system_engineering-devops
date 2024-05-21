af2d0976d36cfef534b89877fb4bc7d6046fbadc
DYung26 <theadstore1407@gmail.com>
2024-05-20 19:18:46 -0700

    a Python script that, using the `JSONPlaceholder` API, for a given employee ID, returns information about his/her TODO list progress.

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A script to fetch and display the number of completed tasks for a given user
+from the JSONPlaceholder API.
+The script takes a user ID as a command-line argument and retrieves the user's
+details and their to-do list.
+It then counts the number of completed tasks and prints them along with the
+task titles.
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    import json
+
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    r = requests.get(f'{base_url}/users/{sys.argv[1]}/').json()
+    r_todo = requests.get(f'{base_url}/users/{sys.argv[1]}/todos').json()
+
+    done_t = 0
+    for todo in r_todo:
+        if todo["completed"]:
+            done_t += 1
+
+    print(f'Employee {r["name"]} is done with tasks({done_t}/{len(r_todo)}):')
+    for todo in r_todo:
+        if todo["completed"]:
+            print(f'\t {todo["title"]}')
