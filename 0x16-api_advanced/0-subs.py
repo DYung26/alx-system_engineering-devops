8a2f5433c7127369334a64540a83e8735dfe48ea
DYung26 <theadstore1407@gmail.com>
2024-08-15 12:27:38 -0700

    a recursive function that queries the `Reddit API` and returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit, the function returns `None`

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,5 +20,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return (response.json()["data"]["subscribers"])
-    else:
+    elif response.status_code == 404:
         return (0)
